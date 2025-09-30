2c66cd8a7d4ff8ce56ac92543e9092b34baa67c6
aansell <aansell@pps.net>
2025-09-30 14:30:06 -0700

    fireball shooting

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -1,15 +1,23 @@
 extends Area2D
-
+var direction 
+var speed = 300
 
 func _ready() -> void:
 	pass
 
 func _physics_process(_delta):
-	
+	position += direction * speed * _delta
 	
 	pass
 
 func set_direction(facing_direction):
-	
+	if facing_direction == "up":
+		direction = Vector2.UP
+	elif facing_direction == "down":
+		direction = Vector2.DOWN
+	elif facing_direction == "left":
+		direction = Vector2.LEFT
+	elif facing_direction == "right":
+		direction = Vector2.RIGHT
 	
 	pass
