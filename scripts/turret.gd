af85a23d1059e8f5a46ffa513269507d00099a9e
aansell <aansell@pps.net>
2025-09-30 14:31:08 -0700

    added on body exit to turret

--- scripts/turret.gd
+++ scripts/turret.gd
@@ -15,3 +15,7 @@ func _process(delta: float) -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	
 	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
