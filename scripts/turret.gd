9e67189bb833949ea4aa3cef2ee0beb058025fec
aansell <aansell@pps.net>
2025-09-30 09:27:39 -0700

    ready for turrets

--- scripts/turret.gd
+++ scripts/turret.gd
@@ -2,9 +2,15 @@ extends CharacterBody2D
 
 
 func _ready():
-	
+	_on_area_2d_body_entered.connect(_on_area_2d_body_entered)
 	pass
 
 func _process(delta: float) -> void:
 	pass
+
+
 	
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
