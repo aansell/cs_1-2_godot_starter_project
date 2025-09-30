5fd326d8154fd67632ab3269ec3788fe2a886d15
aansell <aansell@pps.net>
2025-09-30 13:31:24 -0700

    ready for class to download

--- scripts/enemy_projectile.gd
+++ scripts/enemy_projectile.gd
@@ -1,1 +1,7 @@
-extends Node
+extends Area2D
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body):
+	pass
--- scripts/turret.gd
+++ scripts/turret.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 func _ready():
-	_on_area_2d_body_entered.connect(_on_area_2d_body_entered)
+	
 	pass
 
 func _process(delta: float) -> void:
@@ -13,4 +13,5 @@ func _process(delta: float) -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	
 	pass # Replace with function body.
