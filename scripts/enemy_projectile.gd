27c80d53d4bd2d4c5392b347458f602ec60dc986
ajackson6826 <ajackson6826@student.pps.net>
2025-10-03 10:00:38 -0700

    Turret code

--- scripts/enemy_projectile.gd
+++ scripts/enemy_projectile.gd
@@ -1,7 +1,16 @@
 extends Area2D
+var direction 
+var speed = 300
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
-	pass
+	if body.name == "Player":
+		body.change_health(-2)
+		queue_free()
+
+func _physics_process(_delta):
+	position += direction * speed * _delta
+func set_direction(target):
+	direction = position.direction_to(target)
